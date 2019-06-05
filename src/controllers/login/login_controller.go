a28cfcfc26e00e3d3cf2d8a2c74447bac288ab09
boniface <boniface@kabaso.com>
2019-06-05 18:51:35 +0200

    Initial commit

---
+++ src/controllers/login/login_controller.go
@@ -0,0 +1,85 @@
+package login
+
+import (
+	"github.com/go-chi/chi"
+	"html/template"
+	"net/http"
+	"obas/src/config"
+)
+
+// Route Path
+func Login(app *config.Env) http.Handler {
+	r := chi.NewRouter()
+	r.Get("/", loginHandler(app))
+	r.Post("/accounts", getAccountsHandler(app))
+	r.Get("/password", passwordHandler(app))
+	r.Get("/verify", passwordHandler(app))
+	return r
+
+}
+
+func loginHandler(app *config.Env) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		files := []string{
+			app.Path + "/login/login.page.html",
+		}
+
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			app.ErrorLog.Println(err.Error())
+			return
+		}
+		err = ts.Execute(w, nil)
+		if err != nil {
+			app.ErrorLog.Println(err.Error())
+
+		}
+
+	}
+
+}
+
+func logout(app *config.Env) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		w.Write([]byte("welcome"))
+
+	}
+}
+
+func forgotPassword(app *config.Env) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		w.Write([]byte("welcome"))
+
+	}
+}
+
+func passwordHandler(app *config.Env) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		files := []string{
+			app.Path + "/login/password.page.html",
+		}
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			app.ErrorLog.Println(err.Error())
+			return
+		}
+		err = ts.Execute(w, nil)
+		if err != nil {
+			app.ErrorLog.Println(err.Error())
+		}
+
+	}
+
+}
+
+func getAccountsHandler(app *config.Env) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+
+	}
+}
