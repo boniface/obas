2f738b58cb28de2e3c4edb726540333c095e539e
christian <muamba90@gmail.com>
2019-07-21 23:11:57 +0200

    making few changes on the view repo.  making all the pages to be consistent in terms the style and feels

--- src/io/login/login_io.go
+++ src/io/login/login_io.go
@@ -3,100 +3,129 @@ package io
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
 	"obas/src/api"
-	domain "obas/src/domain/login"
-	"time"
 )
 
-func checkUserName(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-	ctx := appengine.NewContext(req)
-	bs, err := ioutil.ReadAll(req.Body)
-	sbs := string(bs)
-	log.Infof(ctx, "REQUEST BODY: %v", sbs)
-	var user User
-	key := datastore.NewKey(ctx, "Users", sbs, 0, nil)
-	err = datastore.Get(ctx, key, &user)
-	// if there is an err, there is NO user
-	log.Infof(ctx, "ERR: %v", err)
-	if err != nil {
-		// there is an err, there is a NO user
-		fmt.Fprint(res, "false")
-		return
-	} else {
-		fmt.Fprint(res, "true")
+const roleurl = api.BASE_URL + "/login"
+
+type User users.User
+type MessageResponse mail.MessageResponse
+type UserGeneratedToken security.UserGeneratedToken
+type Account users.Account
+type Profile login.Profile
+type LoginCredential login.LoginCredential
+
+func ResetPasswordRequest(resetKey string) (MessageResponse, error) {
+	entity := MessageResponse{}
+	resp, _ := api.Rest().Get(roleurl + "/reset/" + resetKey)
+	if resp.IsError() {
+		return entity, errors.New(resp.Status())
 	}
+	err := json.Unmarshal(resp.Body(), &entity)
+	if err != nil {
+		return entity, errors.New(resp.Status())
+	}
+	return entity, nil
+
 }
 
-func createUser(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-	ctx := appengine.NewContext(req)
-	user := User{
-		Email:    req.FormValue("email"),
-		UserName: req.FormValue("userName"),
-		Password: req.FormValue("password"),
-	}
-	key := datastore.NewKey(ctx, "Users", user.UserName, 0, nil)
-	key, err := datastore.Put(ctx, key, &user)
-	if err != nil {
-		log.Errorf(ctx, "error adding todo: %v", err)
-		http.Error(res, err.Error(), 500)
-		return
+func IsUserRegistered(user User) (bool, error) {
+	resp, _ := api.Rest().
+		SetBody(user).
+		Post(roleurl + "/registered")
+	if resp.IsError() {
+		return false, errors.New(resp.Status())
 	}
 
-	createSession(res, req, user)
-	// redirect
-	http.Redirect(res, req, "/", 302)
+	return true, nil
+
 }
 
-func login(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-	ctx := appengine.NewContext(req)
-	key := datastore.NewKey(ctx, "Users", req.FormValue("userName"), 0, nil)
-	var user User
-	err := datastore.Get(ctx, key, &user)
-	if err != nil || req.FormValue("password") != user.Password {
-		// failure logging in
-		var sd sessionData
-		sd.LoginFail = true
-		tpl.ExecuteTemplate(res, "login.html", sd)
-		return
-	} else {
-		user.UserName = req.FormValue("userName")
-		// success logging in
-		createSession(res, req, user)
-		// redirect
-		http.Redirect(res, req, "/", 302)
+func ForgotPassword(profile Profile) (MessageResponse, error) {
+	entity := MessageResponse{}
+	resp, _ := api.Rest().
+		SetBody(profile).
+		Post(roleurl + "/forgotpassword")
+	if resp.IsError() {
+		return entity, errors.New(resp.Status())
 	}
+	err := json.Unmarshal(resp.Body(), &entity)
+	if err != nil {
+		return entity, errors.New(resp.Status())
+	}
+	return entity, nil
+
 }
 
-func createSession(res http.ResponseWriter, req *http.Request, user User) {
-	ctx := appengine.NewContext(req)
-	// SET COOKIE
-	id, _ := uuid.NewV4()
-	cookie := &http.Cookie{
-		Name:  "session",
-		Value: id.String(),
-		Path:  "/",
-		//		UNCOMMENT WHEN DEPLOYED:
-		//		Secure: true,
-		//		HttpOnly: true,
+func GetUserAccounts(email string) ([]Account, error) {
+	entities := []Account{}
+	resp, _ := api.Rest().Get(roleurl + "/user/" + email)
+	if resp.IsError() {
+		return entities, errors.New(resp.Status())
 	}
-	http.SetCookie(res, cookie)
-
-	// SET MEMCACHE session data (sd)
-	json, err := json.Marshal(user)
+	err := json.Unmarshal(resp.Body(), &entities)
 	if err != nil {
-		log.Errorf(ctx, "error marshalling during user creation: %v", err)
-		http.Error(res, err.Error(), 500)
-		return
+		return entities, errors.New(resp.Status())
 	}
-	sd := memcache.Item{
-		Key:   id.String(),
-		Value: json,
-		//		Expiration: time.Duration(20*time.Minute),
-		Expiration: time.Duration(20 * time.Second),
+	return entities, nil
+
+}
+
+func GetLoginToken(loginCredential LoginCredential) (UserGeneratedToken, error) {
+	entity := UserGeneratedToken{}
+	resp, _ := api.Rest().
+		SetBody(loginCredential).
+		Post(roleurl + "/getlogintoken")
+	if resp.IsError() {
+		return entity, errors.New(resp.Status())
 	}
-	memcache.Set(ctx, &sd)
+	err := json.Unmarshal(resp.Body(), &entity)
+	if err != nil {
+		return entity, errors.New(resp.Status())
+	}
+	return entity, nil
+
+}
+
+func IsUserLoggedIn(user User) (bool, error) {
+	// GET THIS FROM SESSION
+	//resp, _ := api.Rest().
+	//	SetBody(entity).
+	//	Post(roleurl + "/create")
+	//if resp.IsError() {
+	//	return false, errors.New(resp.Status())
+	//}
+
+	return true, nil
+
+}
+
+func GetUserRole(user User) (string, error) {
+	// GET THIS FROM THE SESSION
+
+	//role := Role{}
+	//resp, _ := api.Rest().Get(roleurl + "/get/" + id)
+	//if resp.IsError() {
+	//	return role , errors.New(resp.Status())
+	//}
+	//err := json.Unmarshal(resp.Body(), &role)
+	//if err != nil {
+	//	return role ,  errors.New(resp.Status())
+	//}
+	return "", nil
+
+}
+
+func Logout() (bool, error) {
+	// INVALIDATE THE SESSION
+
+	//resp, _ := api.Rest().
+	//	SetBody(entity).
+	//	Post(roleurl + "/create")
+	//if resp.IsError() {
+	//	return false, errors.New(resp.Status())
+	//}
+
+	return true, nil
+
 }
