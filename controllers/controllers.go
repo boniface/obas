0f8ff90651c5b4ab6c821559befd6afb3330454b
boniface <boniface@kabaso.com>
2019-09-24 14:20:22 +0200

    Fixed the CSS Refactoring

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -39,7 +39,7 @@ func Controllers(env *config.Env) http.Handler {
 	mux.Mount("/location", controllers9.Locations(env))
 	mux.Mount("/log", controllers10.Logs(env))
 
-	fileServer := http.FileServer(http.Dir("./src/views/assets/"))
+	fileServer := http.FileServer(http.Dir("./views/assets/"))
 	// Use the mux.Handle() function to register the file server as the handler for
 	// all URL paths that start with "/assets/". For matching paths, we strip the
 	// "/static" prefix before the request reaches the file server.
