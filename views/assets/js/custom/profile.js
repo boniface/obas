271c3281ac0b09e1a04aed47cd6e81372feccccd
sizwe.ncikana@gmail.com <japhetmibatu1@gmail.com>
2019-10-04 14:44:51 +0200

    chamge the colors of tabs icon(espoir)

--- views/assets/js/custom/profile.js
+++ views/assets/js/custom/profile.js
@@ -6,5 +6,9 @@ $(document).ready(function(){
         $("#profileForm input").prop("disabled", false);
     });
 
+    $("#clearButton").click(function () {
+        $("#profileForm first").empty()
+       // document.getElementById("myForm").reset();
+    })
 
 });
