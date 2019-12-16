6d781ae119172539c0af4e0c87c74dc6bd5089ba
caniksea <caniksea@yahoo.co.nz>
2019-12-16 12:41:06 +0200

    Merged @Espoir's input

--- io/institutions/institution.go
+++ io/institutions/institution.go
@@ -49,14 +49,17 @@ func GetInstitution(id string) (domain.Institution, error) {
 
 func GetInstitutions() ([]domain.Institution, error) {
 	entity := []domain.Institution{}
-	resp, _ := api.Rest().Get(institutionURL + "/all")
-	if resp.IsError() {
-		return entity, errors.New(resp.Status())
-	}
-	err := api.JSON.Unmarshal(resp.Body(), &entity)
-	if err != nil {
-		return entity, errors.New(resp.Status())
-	}
+	entity = append(entity, domain.Institution{"1", "1", "CPUT"})
+	entity = append(entity, domain.Institution{"2", "1", "UWC"})
+	entity = append(entity, domain.Institution{"3", "1", "Stellenbosch"})
+	//resp, _ := api.Rest().Get(institutionURL + "/all")
+	//if resp.IsError() {
+	//	return entity, errors.New(resp.Status())
+	//}
+	//err := api.JSON.Unmarshal(resp.Body(), &entity)
+	//if err != nil {
+	//	return entity, errors.New(resp.Status())
+	//}
 	return entity, nil
 }
 
--- io/login/login_io.go
+++ io/login/login_io.go
@@ -50,18 +50,18 @@ func DoReset(resetKey string) (bool, error) {
 
 func DoLogin(email string, password string) (LoginToken, error) {
 	entity := Login{email, password}
-	resp, _ := api.Rest().
-		SetBody(entity).
-		Post(loginURL + "/login")
-	if resp.IsError() {
-		return LoginToken{}, errors.New(resp.Status())
-	}
+	//resp, _ := api.Rest().
+	//	SetBody(entity).
+	//	Post(loginURL + "/login")
+	//if resp.IsError() {
+	//	return LoginToken{}, errors.New(resp.Status())
+	//}
 	respEntity := LoginToken{}
-	//respEntity = LoginToken{entity.Email, "aerefasd.foqerwfdasdfaoduo"}
-	err := json.Unmarshal(resp.Body(), &respEntity)
-	if err != nil {
-		return respEntity, errors.New(resp.Status())
-	}
+	respEntity = LoginToken{entity.Email, "aerefasd.foqerwfdasdfaoduo"}
+	//err := json.Unmarshal(resp.Body(), &respEntity)
+	//if err != nil {
+	//	return respEntity, errors.New(resp.Status())
+	//}
 	return respEntity, nil
 }
 
