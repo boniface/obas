5f59ff41b6631d0e0ffdaae9131345e43f5184e6
caniksea <caniksea@yahoo.co.nz>
2019-12-24 16:34:31 +0200

    Feature added ==> Implemented update of applicant type on application. ==> Completed matric institution CRUD

--- io/users/userInstitution_io.go
+++ io/users/userInstitution_io.go
@@ -3,51 +3,36 @@ package users
 import (
 	"errors"
 	"obas/api"
-	domain "obas/domain/users"
 )
 
 const userInstitutionURL = api.BASE_URL + "/users/institution/"
 
-func GetUserInstitutions() ([]domain.UserInstitution, error) {
-	entites := []domain.UserInstitution{}
-	resp, _ := api.Rest().Get(userInstitutionURL + "/institution/all")
+//func GetUserInstitutions() ([]domain.UserInstitution, error) {
+//	entites := []domain.UserInstitution{}
+//	resp, _ := api.Rest().Get(userInstitutionURL + "/institution/all")
+//
+//	if resp.IsError() {
+//		return entites, errors.New(resp.Status())
+//	}
+//	err := api.JSON.Unmarshal(resp.Body(), &entites)
+//	if err != nil {
+//		return entites, errors.New(resp.Status())
+//	}
+//	return entites, nil
+//}
 
-	if resp.IsError() {
-		return entites, errors.New(resp.Status())
-	}
-	err := api.JSON.Unmarshal(resp.Body(), &entites)
-	if err != nil {
-		return entites, errors.New(resp.Status())
-	}
-	return entites, nil
-}
-
-func GetUserInstitution(id string) (domain.UserInstitution, error) {
-	entity := domain.UserInstitution{}
-	resp, _ := api.Rest().Get(userInstitutionURL + "/institution/get/" + id)
-	if resp.IsError() {
-		return entity, errors.New(resp.Status())
-	}
-	err := api.JSON.Unmarshal(resp.Body(), &entity)
-	if err != nil {
-		return entity, errors.New(resp.Status())
-	}
-	return entity, nil
-}
-
-func CreateUserInstitution(entity domain.UserInstitution) (domain.UserInstitution, error) {
-	resp, _ := api.Rest().
-		SetBody(entity).
-		Post(userInstitutionURL + "create")
-	if resp.IsError() {
-		return entity, errors.New(resp.Status())
-	}
-	err := api.JSON.Unmarshal(resp.Body(), &entity)
-	if err != nil {
-		return entity, errors.New(resp.Status())
-	}
-	return entity, nil
-}
+//func GetUserInstitution(id string) (domain.UserInstitution, error) {
+//	entity := domain.UserInstitution{}
+//	resp, _ := api.Rest().Get(userInstitutionURL + "/institution/get/" + id)
+//	if resp.IsError() {
+//		return entity, errors.New(resp.Status())
+//	}
+//	err := api.JSON.Unmarshal(resp.Body(), &entity)
+//	if err != nil {
+//		return entity, errors.New(resp.Status())
+//	}
+//	return entity, nil
+//}
 
 func UpdateUserInstitution(entity interface{}) (bool, error) {
 	resp, _ := api.Rest().
